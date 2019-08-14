c4577d362a4d51bee8fc3fea5a5f2d57e244fe0d
Oneil Williams <oneilswilliams@gmail.com>
2019-08-14 21:48:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,8 @@
 def using_push(array,element)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+  #colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  #next_color = "violet"
+  #colors_in_the_rainbow.push(next_color)
+  array.push(element)
 end 
 
 def using_unshift(array,element)
