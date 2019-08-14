de55e1d4221fc7053cbc3ded442d9706d0daa5eb
Oneil Williams <oneilswilliams@gmail.com>
2019-08-14 21:17:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,7 @@ def using_pop(array)
   continents.pop
 end 
 
-def pop_with_args(dog_breeds)
+def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   small_dogs = dog_breeds.pop(2) 
 end 
